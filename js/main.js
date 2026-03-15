48e8a09e7e00bc4a6e25fe164209761975c43863
theakim <theakim200@gmail.com>
2026-03-15 18:01:23 -0400

    vh

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+function setVh() {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+setVh();
+window.addEventListener('resize', setVh);
+
 // const isMobile = /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
 const isMobile = window.innerWidth <= 768;
 
